4c835179b21065536e75675e96c25915a9927567
hubby12a <paolauwase555@gmail.com>
2025-11-22 20:55:39 +0200

    added successfully

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,31 +14,31 @@ const menuItems = [
       {
         icon: "/teacher.png",
         label: "Teachers",
-        href: "/list/teachers",
+        href: "/teacher",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/student.png",
         label: "Students",
-        href: "/list/students",
+        href: "/students",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/parent.png",
         label: "Parents",
-        href: "/list/parents",
+        href: "/parent",
         visible: ["admin", "teacher"],
       },
       {
         icon: "/subject.png",
         label: "Subjects",
-        href: "/list/subjects",
+        href: "/subject",
         visible: ["admin"],
       },
       {
         icon: "/class.png",
         label: "Classes",
-        href: "/list/classes",
+        href: "/classes",
         visible: ["admin", "teacher"],
       },
       {
